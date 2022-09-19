46656520a2a4836697d94a7c4863b159afcf1e87
Oskar <37629838+Badmondy@users.noreply.github.com>
2022-09-19 17:18:03 +0200

    Add files via upload

---
+++ FryeleAdaptiv/main.js
@@ -0,0 +1,30 @@
+function homePage(){  // Hem - start sida!
+    window.location.href = "index.html";
+}
+function boardPage(){  // Föreningen -- > bli medlem(signupPage) är separat!
+    window.location.href = "board.html";
+}
+
+function signupPage(){ // bli medlem i föreningen!
+    window.location.href = "signup.html";
+}
+
+function horsesPage(){ // Hästarna -- > Personal -- > Information
+    window.location.href = "horses.html";
+}
+
+function personalPage(){ // Personal sida!
+    window.location.href = "personal.html";
+}
+
+function informationPage(){ // Information sida!
+    window.location.href = "information.html";
+}
+
+function propertyPage(){ // Anläggningsskiss!
+    window.location.href = "property.html";
+}
+
+function contactPage() {  // Som bli medlem fast kontakt formulär!
+    window.location.href = "contact.html";
+}
